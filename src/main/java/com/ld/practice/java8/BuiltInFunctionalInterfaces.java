7f6fbcec43c756cba027b0a1d76fe6db7349cab4
LD00000 <lidong9144@163.com>
2016-04-30 00:58:22 +0800

    java 8 内置函数

---
+++ src/main/java/com/ld/practice/java8/BuiltInFunctionalInterfaces.java
@@ -0,0 +1,23 @@
+package com.ld.practice.java8;
+
+/**
+ * Java 8 内置函数
+ * 
+ * JDK 1.8 API包含了许多内置函数. 它们中很多在老版本中是大家很熟悉的,像 Comparator 和Runnable. 
+ * 这些接口通过 添加@FunctionalInterface注解来支持Lambda表达式.
+ *
+ * 但是 Java 8 API 也添加了很多新接口使编程变的更加容易. 这些新的函数很多是借鉴被大家所熟知的 Google Guava 函数库
+ */
+public class BuiltInFunctionalInterfaces {
+	
+	/*
+	 * ===============================================
+	 * Predicates( 判断)
+	 * 判断是传递一个参数返回一个布尔值. 这个接口包含各种默认的方法组成复杂的逻辑判断单元 (and, or, negate)
+	 * ===============================================
+	 */
+	public void predicatesEG() {
+		
+	}
+	
+}
