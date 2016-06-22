ce9595e44d88c2d7be0b0ddb313f4acc553653d5
ld0000 <lidong9144@163.com>
2016-06-22 09:46:12 +0800

    位操作

--- src/main/java/com/ld/practice/enumprac/EnumOrdinal.java
+++ src/main/java/com/ld/practice/enumprac/EnumOrdinal.java
@@ -10,8 +10,17 @@ public enum EnumOrdinal {
 	Test3;
 	
 	public static void main(String[] args) {
-		System.out.println(EnumOrdinal.Test1.ordinal());
-		System.out.println(EnumOrdinal.Test2.ordinal());
+//		System.out.println(EnumOrdinal.Test1.ordinal());
+//		System.out.println(EnumOrdinal.Test2.ordinal());
+//		
+//		System.out.println("----------------------");
+//		System.out.println(1 << 0);
+//		System.out.println(1 << 1);
+//		System.out.println(1 << 2);
+		
+		System.out.println(2 | 4);
+		System.out.println(6 | 8);
+		System.out.println(14 | 16);
 	}
 	
 }
